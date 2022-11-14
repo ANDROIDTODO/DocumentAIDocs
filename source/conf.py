c53d3168618de4f9b6c217fdf103918acb4fd99f
yangjun <yangjun@kdanmobile.com>
2022-11-14 13:51:34 +0800

    2.0.1-rc

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, JeromeYang'
 author = 'JeromeYang'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '2.0.1-rc'
 
 
 # -- General configuration ---------------------------------------------------
